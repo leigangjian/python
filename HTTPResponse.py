485d138b9f5b0d797a97d0c70da51be7fcd710e7
leigangjian <3032507811@qq.com>
2021-01-28 17:03:22 +0800

    aha

---
+++ HTTPResponse.py
@@ -0,0 +1,8 @@
+import urllib.request                           #导入request子模块
+url = 'https://fishc.com.cn/forum.php'
+response = urllib.request.urlopen(url = url)    #发送网络请求
+print('状态响应码为： ', response.status)
+print('响应头所有信息为： ',response.getheaders())
+print('响应头指定信息为： ',response.getheader('Accept-Ranges'))
+#读取HTML代码并进行utf-8解码
+print('Python官网HTML代码如下：\n',response.read().decode('utf-8'))
